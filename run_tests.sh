3e0ea3eca3fea360608e5d7095dc07cad05f8bce
Julian Finkler <julian@mintware.de>
2021-10-23 11:28:24 +0200

    Tests fixed

--- run_tests.sh
+++ run_tests.sh
@@ -5,12 +5,13 @@ cd ./example
 pub get
 rm -f lib/src/example.container.dart
 pub run build_runner build --delete-conflicting-outputs
-sed -i'' "s+package\:catalyst_builder_example\/src\/++g" "lib/src/example.container.dart"
-sed -i'' "s+package\:third_party_dependency\/+\.\.\/\.\.\/\.\.\/test\/third_party_dependency\/lib\/+g" "lib/src/example.container.dart"
+sed -i '' "s+package\:catalyst_builder_example\/src\/++g" "lib/src/example.container.dart"
+sed -i '' "s+package\:third_party_dependency\/+\.\.\/\.\.\/\.\.\/test\/third_party_dependency\/lib\/+g" "lib/src/example.container.dart"
 cd ..
 
 dart test
 
+REPO_TOKEN=$1
 if [ "$REPO_TOKEN" ]; then
   pub global activate dart_coveralls
   pub global run dart_coveralls report \
