6448ae887cede11eda8b948ffb26ab9ca734ccf9
Julian Finkler <julian@mintware.de>
2021-10-23 10:58:23 +0200

    Fix ci

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,8 @@
 cd ./example;
 pub get
 rm -f lib/src/example.container.dart
-pub run build_runner build --delete-conflicting-outputs;
+pub run build_runner build --delete-conflicting-outputs
+pwd
 sed -i '' "s+package\:catalyst_builder_example\/src\/++g" "lib/src/example.container.dart"
 sed -i '' "s+package\:third_party_dependency\/+\.\.\/\.\.\/\.\.\/test\/third_party_dependency\/lib\/+g" "lib/src/example.container.dart"
 cd ..
