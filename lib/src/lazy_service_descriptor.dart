d4dd00104f69aa939b788bceda7982f99673f3fd
Julian Finkler <julian@mintware.de>
2022-04-20 16:32:30 +0200

    Added doc comment

--- lib/src/lazy_service_descriptor.dart
+++ lib/src/lazy_service_descriptor.dart
@@ -1,8 +1,17 @@
 import '../catalyst_builder.dart';
 
+/// A description of a lazy loaded service
 class LazyServiceDescriptor<T> {
+  /// The service description.
   final Service service;
+
+  /// The factory to produce the service.
   final T Function(ServiceProvider) factory;
 
-  LazyServiceDescriptor(this.factory, [this.service = const Service()]);
+  /// Creates a new [LazyServiceDescriptor] which produces a [T]
+  /// using the [factory].
+  LazyServiceDescriptor(
+    this.factory, [
+    this.service = const Service(),
+  ]);
 }
