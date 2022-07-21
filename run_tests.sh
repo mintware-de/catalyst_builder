46f8dc65e50918a57d7a4497c78a20457188d00d
Julian Finkler <julian@mintware.de>
2022-07-21 16:41:06 +0200

    Removed coverage report since dart_coveralls is not working anymore

--- run_tests.sh
+++ run_tests.sh
@@ -11,13 +11,3 @@ cat "lib/src/example.container.dart" \
 cd ..
 
 dart run test
-
-REPO_TOKEN=$1
-if [ "$REPO_TOKEN" ]; then
- dart pub global activate dart_coveralls
- dart pub global run dart_coveralls report \
-    --token $REPO_TOKEN \
-    --retry 2 \
-    --exclude-test-files \
-    test/coveralls.dart
-fi
