ea57325f0fa0728ae7905a9003b59d032798b1b2
Julian Finkler <julian@mintware.de>
2023-05-26 15:33:55 +0200

    change(Example): Created a public_api.dart
    This simplifies testing

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -1,29 +1,10 @@
-library catalyst_builder_example;
-
-import 'dart:convert';
-
 import 'package:catalyst_builder/catalyst_builder.dart';
 
 import './src/manually_wired_service.dart';
 
+export './public_api.dart';
 export './src/manually_wired_service.dart';
 export 'example.catalyst_builder.g.dart';
-
-part './src/chat_provider.dart';
-
-part './src/cool_chat_provider.dart';
-
-part './src/preload_service.dart';
-
-part './src/singleton_service.dart';
-
-part './src/transient_service.dart';
-
-part './src/transport.dart';
-
-part './src/self_registered_service.dart';
-
-part './src/broadcaster.dart';
 
 @Preload()
 @GenerateServiceProvider()
