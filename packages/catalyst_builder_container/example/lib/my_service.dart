aaa314de6cf0a3c442c9a9b92a2ced808fd67f09
Julian Finkler <julian@mintware.de>
2025-04-14 05:11:29 +0200

    feat: Add the catalyst_builder_container package

---
+++ packages/catalyst_builder_container/example/lib/my_service.dart
@@ -0,0 +1,8 @@
+import 'package:catalyst_builder_contracts/catalyst_builder_contracts.dart';
+
+@Service()
+class MyService {
+  void sayHello() {
+    print('Hello World');
+  }
+}
---
+++ packages/catalyst_builder_container/lib/catalyst_builder_container.dart
@@ -0,0 +1,3 @@
+library;
+
+export 'src/service_container.dart';
