08e3efb932e989300574e2ea0a98d3b9dba92ea3
Julian Finkler <julian@mintware.de>
2025-04-15 07:05:07 +0200

    fix: formatting

--- packages/catalyst_builder/lib/src/builder/constants.dart
+++ packages/catalyst_builder/lib/src/builder/constants.dart
@@ -1,5 +1,6 @@
 const String preflightExtension = '.catalyst_builder.preflight.json';
 const String oldAnnotationsPrefix = 'package:catalyst_builder/src/annotation/';
-const String serviceContainerPluginExtension = '.catalyst_builder.plugin.g.dart';
+const String serviceContainerPluginExtension =
+    '.catalyst_builder.plugin.g.dart';
 const String annotationsPrefix =
     'package:catalyst_builder_contracts/src/annotation/';
