d555928e201c3946e056a4b784a08873114a8964
Julian Finkler <julian@mintware.de>
2025-10-12 10:37:58 +0200

    release: v6.0.0-dev.2

--- packages/catalyst_builder/lib/src/builder/helpers.dart
+++ packages/catalyst_builder/lib/src/builder/helpers.dart
@@ -5,10 +5,10 @@ import 'constants.dart';
 extension ElementAnnotationExtension on ElementAnnotation {
   /// Checks if the annotation is part of the catalyst_builder package.
   bool isLibraryAnnotation(String name) {
-    if (element2?.enclosingElement2?.displayName != name) {
+    if (element?.enclosingElement?.displayName != name) {
       return false;
     }
-    var packageUri = element2!.library2?.uri.toString();
+    var packageUri = element!.library?.uri.toString();
     if (packageUri == null) {
       return false;
     }
